81483deba1e89b9e55eacdfa54aa2a6d4be0eefb
Kishan <kishan.mishra@accolitedigital.com>
2021-05-27 02:08:24 +0530

    theme added, global fonts, fontSize, colors, images added

---
+++ FoodDeliveryApp/constants/index.js
@@ -0,0 +1,8 @@
+import images from "./images";
+import icons from "./icons";
+import { COLORS, SIZES, FONTS } from './themes';
+import GOOGLE_API_KEY from './maps';
+
+export {images, icons, COLORS, SIZES, FONTS, GOOGLE_API_KEY};
+
+// We import here everything and use export it from index.js so that it is usable everywhere
---
+++ FoodDeliveryApp/constants/maps.js
@@ -0,0 +1,4 @@
+// We are having a Google api key for google maps
+const GOOGLE_API_KEY = "AIzaSyD_lMXvGFGsiO2qhvX7Vs49r-sdICRtrAQ"
+
+export default GOOGLE_API_KEY
